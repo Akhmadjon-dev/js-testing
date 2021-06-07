1e89834b36709658b3b72f7d3100a1df3abe5a94
akhmadjon-dev <info.ahmadjon@gmail.com>
2021-06-07 18:19:29 +0500

    two unit test created

--- util.test.js
+++ util.test.js
@@ -2,5 +2,9 @@ const { generateText } = require("./util");
 
 test("should output name and age", () => {
   const text = generateText("Akhmadjon", 22);
-  expect(text).tobe("");
+  expect(text).toBe("Akhmadjon (22 years old)");
+});
+test("should not be valid response", () => {
+  const text = generateText("", null);
+  expect(text).toBe(" (null years old)");
 });
